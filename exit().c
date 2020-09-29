03ce35a5f481248c436b16e60afb594ff7e72b39
Rishita Mukherjee <51188766+rishita2701@users.noreply.github.com>
2020-09-29 15:36:54 +0530

    Create exit().c

---
+++ exit().c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("START");  
+    exit(0);
+   // The program is terminated here   
+   // This line is not printed  
+    printf("END");
+}
